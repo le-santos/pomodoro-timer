2b1238cf5e2cfaee9368c79040b25bb4b4b62d6c
le-santos <lequinterio@gmail.com>
2020-07-07 11:36:51 -0300

    Changed counter variables and indentation

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-const workTime = {
-	min: 25,
-	sec: 0
-};
-
-const restTime = {
-	min: 05,
-	sec: 0
+const counters = {
+  work: { min: 25, sec: 0 },
+  rest: { min: 05, sec: 0 },
 };
 
 const timer = document.getElementById("work-time");
 const rest = document.getElementById("rest-time");
 
-// Creating the string to show 2 digits number
+// Creating the string for the 2 digits format (00:00)
 
 let setDisplayNum = (m, s, clock) => {
-	m = ("00" + m).slice(-2);
-	s = ("00" + s).slice(-2);
-	clock.innerHTML = `${m}:${s}`;
+  m = ("00" + m).slice(-2);
+  s = ("00" + s).slice(-2);
+  clock.innerHTML = `${m}:${s}`;
 };
 
-setDisplayNum(workTime.min, workTime.sec, timer);
-setDisplayNum(restTime.min, restTime.sec, rest);
+setDisplayNum(counters.work.min, counters.work.sec, timer);
+setDisplayNum(counters.rest.min, counters.rest.sec, rest);
 
 // Main function
 
 const count = function () {
-	if (workTime.min > 0 && workTime.sec === 0) {
-		workTime.min--;
-		workTime.sec = 59;
-	} else if (workTime.sec > 0) {
-		workTime.sec--;
-	} else {
-		if (restTime.min > 0 && restTime.sec === 0) {
-			restTime.min--;
-			restTime.sec = 59;
-		} else if (restTime.sec > 0) {
-			restTime.sec--;
-		}
-	}
-	setDisplayNum(workTime.min, workTime.sec, timer);
-	setDisplayNum(restTime.min, restTime.sec, rest);
+  if (counters.work.min > 0 && counters.work.sec === 0) {
+    counters.work.min--;
+    counters.work.sec = 59;
+  } else if (counters.work.sec > 0) {
+    counters.work.sec--;
+  } else {
+    if (counters.rest.min > 0 && counters.rest.sec === 0) {
+      counters.rest.min--;
+      counters.rest.sec = 59;
+    } else if (counters.rest.sec > 0) {
+      counters.rest.sec--;
+    }
+  }
+  setDisplayNum(counters.work.min, counters.work.sec, timer);
+  setDisplayNum(counters.rest.min, counters.rest.sec, rest);
 };
 
 // Button Selectors and command functions
@@ -51,24 +46,24 @@ const reset = document.getElementById("reset");
 var goTimer; // Variable for the setInterval counter
 
 const getCounting = function () {
-	start.removeEventListener("click", getCounting);
-	goTimer = setInterval(count, 1000);
+  start.removeEventListener("click", getCounting);
+  goTimer = setInterval(count, 1000);
 };
 
 const stopCount = function () {
-	start.addEventListener("click", getCounting);
-	clearInterval(goTimer);
+  start.addEventListener("click", getCounting);
+  clearInterval(goTimer);
 };
 
 const resetCount = function () {
-	workTime.min = 25;
-	workTime.sec = 00;
-	restTime.min = 05;
-	restTime.sec = 00;
-	setDisplayNum(workTime.min, workTime.sec, timer);
-	setDisplayNum(restTime.min, restTime.sec, rest);
+  counters.work.min = 25;
+  counters.work.sec = 00;
+  counters.rest.min = 05;
+  counters.rest.sec = 00;
+  setDisplayNum(counters.work.min, counters.work.sec, timer);
+  setDisplayNum(counters.rest.min, counters.rest.sec, rest);
 
-	clearInterval(goTimer);
+  clearInterval(goTimer);
 };
 
 start.addEventListener("click", getCounting);
@@ -78,38 +73,38 @@ reset.addEventListener("click", resetCount);
 // Functions for adding/removing time from work/rest counters
 
 const addTime = (timeData) => {
-	if (timeData.min === 99) {
-		return;
-	} else {
-		return timeData.min++;
-	}
+  if (timeData.min === 99) {
+    return;
+  } else {
+    return timeData.min++;
+  }
 };
 
 const removeTime = (timeData) => {
-	if (timeData.min === 0) {
-		return;
-	} else {
-		return timeData.min--;
-	}
+  if (timeData.min === 0) {
+    return;
+  } else {
+    return timeData.min--;
+  }
 };
 
 const changeTime = () => {
-	const operation = event.target.name;
-	const areaToChange = event.target.id;
-	let displaySelected;
+  const operation = event.target.name;
+  const areaToChange = event.target.id;
+  let displaySelected;
 
-	areaToChange === "work-more" || areaToChange === "work-less"
-		? (displaySelected = workTime)
-		: (displaySelected = restTime);
+  areaToChange === "work-more" || areaToChange === "work-less"
+    ? (displaySelected = counters.work)
+    : (displaySelected = counters.rest);
 
-	if (operation === "add") {
-		addTime(displaySelected);
-	} else if (operation === "subt") {
-		removeTime(displaySelected);
-	}
+  if (operation === "add") {
+    addTime(displaySelected);
+  } else if (operation === "subt") {
+    removeTime(displaySelected);
+  }
 
-	setDisplayNum(workTime.min, workTime.sec, timer);
-	setDisplayNum(restTime.min, restTime.sec, rest);
+  setDisplayNum(counters.work.min, counters.work.sec, timer);
+  setDisplayNum(counters.rest.min, counters.rest.sec, rest);
 };
 
 document.getElementById("work-more").addEventListener("click", changeTime);
